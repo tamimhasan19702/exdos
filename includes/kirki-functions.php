a55ccebdfcdb0282e1407a92797f0148f305c75e
tamimhasan19702 <tamimhasancu@gmail.com>
2024-12-27 19:22:48 +0600

    update 2 files and delete 1 file

--- includes/kirki-functions.php
+++ includes/kirki-functions.php
@@ -16,7 +16,7 @@ function kirki_panels()
 
 kirki_panels();
 
-function kirki_sections()
+function exdos_header_section()
 {
     if (class_exists('Kirki\Section')) {
         new \Kirki\Section(
@@ -63,4 +63,217 @@ function kirki_sections()
     }
 }
 
-kirki_sections();
+exdos_header_section();
+
+function exdos_offcanvas_menu_section()
+{
+    if (class_exists('Kirki\Section')) {
+        new \Kirki\Section(
+            'exdos_offcanvas_menu_section',
+            [
+                'title' => esc_html__('Exdos Offcanvas Menu Section', 'kirki'),
+                'description' => esc_html__('Exdos Offcanvas Menu Section Description', 'kirki'),
+                'panel' => 'exdos_kirki_panel',
+                'priority' => 160,
+            ]
+        );
+
+        if (class_exists('Kirki\Field')) {
+
+            new \Kirki\Field\Image(
+                [
+                    'settings' => 'exdos_offcanvas_menu_logo',
+                    'label' => esc_html__('Exdos Offcanvas Menu Logo', 'kirki'),
+                    'description' => esc_html__('The saved value will be the URL for the offcanvas menu.', 'kirki'),
+                    'section' => 'exdos_offcanvas_menu_section',
+                    'default' => get_template_directory_uri() . '/assets/img/logo/logo-black.png',
+                ]
+            );
+
+            new \Kirki\Field\Text(
+                [
+                    'settings' => 'exdos_offcanvas_menu_text',
+                    'label' => esc_html__('Offcanvas Menu Text', 'kirki'),
+                    'section' => 'exdos_offcanvas_menu_section',
+                    'default' => esc_html__('Hello There!', 'kirki'),
+                ]
+            );
+
+            new \Kirki\Field\Textarea(
+                [
+                    'settings' => 'exdos_offcanvas_textarea',
+                    'label' => esc_html__('Offcanvas Textarea', 'kirki'),
+                    'section' => 'exdos_offcanvas_menu_section',
+                    'default' => esc_html__('This is the offcanvas default text', 'kirki'),
+                    'description' => esc_html__('Lorem ipsum dolor sit amet, consect etur adipiscing elit.', 'kirki'),
+                ]
+            );
+
+            new \Kirki\Field\Repeater(
+                [
+                    'settings' => 'exdos_offcanvas_gallery',
+                    'label' => esc_html__('Offcanvas Gallery', 'kirki'),
+                    'section' => 'exdos_offcanvas_menu_section',
+                    'priority' => 10,
+                    'row_label' => [
+                        'type' => 'field',
+                        'value' => esc_html__('Image', 'kirki'),
+                        'field' => 'image_url',
+                    ],
+                    'button_label' => esc_html__('Add Image', 'kirki'),
+                    'default' => [
+                        [
+                            'image_url' => get_template_directory_uri() . '/assets/img/gallery/image-1.jpg',
+                            'image_alt' => esc_html__('Image 1', 'kirki'),
+                        ],
+                        [
+                            'image_url' => get_template_directory_uri() . '/assets/img/gallery/image-2.jpg',
+                            'image_alt' => esc_html__('Image 2', 'kirki'),
+                        ],
+                    ],
+                    'fields' => [
+                        'image_url' => [
+                            'type' => 'image',
+                            'label' => esc_html__('Image URL', 'kirki'),
+                            'description' => esc_html__('Select an image for the gallery.', 'kirki'),
+                            'default' => '',
+                        ],
+                        'image_alt' => [
+                            'type' => 'text',
+                            'label' => esc_html__('Image Alt Text', 'kirki'),
+                            'description' => esc_html__('Enter alt text for the image.', 'kirki'),
+                            'default' => '',
+                        ],
+                    ],
+                ]
+            );
+        }
+    }
+}
+
+exdos_offcanvas_menu_section();
+
+function exdos_offcanvas_information_section()
+{
+    if (class_exists('Kirki\Section')) {
+        new \Kirki\Section(
+            'exdos_offcanvas_information_section',
+            [
+                'title' => esc_html__('Exdos Offcanvas Information Section', 'kirki'),
+                'description' => esc_html__('Exdos Offcanvas Information Section Description', 'kirki'),
+                'panel' => 'exdos_kirki_panel',
+                'priority' => 160,
+            ]
+        );
+
+        if (class_exists('Kirki\Field')) {
+
+            new \Kirki\Field\Text(
+                [
+                    'settings' => 'exdos_offcanvas_information_label',
+                    'label' => esc_html__('Offcanvas Information Text', 'kirki'),
+                    'section' => 'exdos_offcanvas_information_section',
+                    'default' => esc_html__('Information', 'kirki'),
+                ]
+            );
+
+            new \Kirki\Field\Text(
+                [
+                    'settings' => 'exdos_offcanvas_phone_number',
+                    'label' => esc_html__('Offcanvas Phone Number', 'kirki'),
+                    'section' => 'exdos_offcanvas_information_section',
+                    'default' => esc_html__('02 (256) 256 025', 'kirki'),
+                ]
+            );
+
+            new \Kirki\Field\Text(
+                [
+                    'settings' => 'exdos_offcanvas_email',
+                    'label' => esc_html__('Offcanvas Email', 'kirki'),
+                    'section' => 'exdos_offcanvas_information_section',
+                    'default' => esc_html__('info@exdos.com', 'kirki'),
+                ]
+            );
+
+            new \Kirki\Field\Text(
+                [
+                    'settings' => 'exdos_offcanvas_location',
+                    'label' => esc_html__('Offcanvas Location', 'kirki'),
+                    'section' => 'exdos_offcanvas_information_section',
+                    'default' => esc_html__('Avenue de Roma 158b, Lisboa', 'kirki'),
+                ]
+            );
+        }
+    }
+}
+
+exdos_offcanvas_information_section();
+
+function exdos_offcanvas_follow_us_section()
+{
+    if (class_exists('Kirki\Section')) {
+        new \Kirki\Section(
+            'exdos_offcanvas_follow_us_section',
+            [
+                'title' => esc_html__('Exdos Offcanvas Follow Us Section', 'kirki'),
+                'description' => esc_html__('Exdos Offcanvas Follow Us Section Description', 'kirki'),
+                'panel' => 'exdos_kirki_panel',
+                'priority' => 160,
+            ]
+        );
+
+        if (class_exists('Kirki\Field')) {
+
+            new \Kirki\Field\Text(
+                [
+                    'settings' => 'exdos_offcanvas_follow_us',
+                    'label' => esc_html__('Offcanvas Follow Us Text', 'kirki'),
+                    'section' => 'exdos_offcanvas_follow_us_section',
+                    'default' => esc_html__('Follow Us', 'kirki'),
+                ]
+            );
+
+            new \Kirki\Field\URL(
+                [
+                    'settings' => 'exdos_offcanvas_facebook',
+                    'label' => esc_html__('Facebook URL', 'kirki'),
+                    'section' => 'exdos_offcanvas_follow_us_section',
+                    'default' => 'https://www.facebook.com/',
+                    'priority' => 10,
+                ]
+            );
+
+            new \Kirki\Field\URL(
+                [
+                    'settings' => 'exdos_offcanvas_instagram',
+                    'label' => esc_html__('Instagram URL', 'kirki'),
+                    'section' => 'exdos_offcanvas_follow_us_section',
+                    'default' => 'https://www.instagram.com/',
+                    'priority' => 10,
+                ]
+            );
+
+            new \Kirki\Field\URL(
+                [
+                    'settings' => 'exdos_offcanvas_linkedin',
+                    'label' => esc_html__('LinkedIn URL', 'kirki'),
+                    'section' => 'exdos_offcanvas_follow_us_section',
+                    'default' => 'https://www.linkedin.com/',
+                    'priority' => 10,
+                ]
+            );
+
+            new \Kirki\Field\URL(
+                [
+                    'settings' => 'exdos_offcanvas_twitter',
+                    'label' => esc_html__('Twitter URL', 'kirki'),
+                    'section' => 'exdos_offcanvas_follow_us_section',
+                    'default' => 'https://twitter.com/',
+                    'priority' => 10,
+                ]
+            );
+        }
+    }
+}
+
+exdos_offcanvas_follow_us_section();
--- template-parts/header/tp-offcanvas.php
+++ template-parts/header/tp-offcanvas.php
@@ -1,11 +1,36 @@
-<?php ?>
+<?php
 
+/**
+ * Offcanvas template part
+ *
+ * @package Exdos
+ */
+
+// offcanvas menu section
+$off_canvas_logo = get_theme_mod('exdos_offcanvas_menu_logo', get_template_directory_uri() . '/assets/img/logo/logo-black.png');
+$off_canvas_text = get_theme_mod('exdos_offcanvas_menu_text', 'Hello There!');
+$off_canvas_description = get_theme_mod('exdos_offcanvas_textarea', 'Lorem ipsum dolor sit amet, consect etur adipiscing elit. ');
+$off_canvas_repeater = get_theme_mod('exdos_offcanvas_gallery', get_template_directory_uri() . '/assets/img/project/showcase-thumb-01.jpg');
+
+// offcanvas information
+$off_canvas_information = get_theme_mod('exdos_offcanvas_information_label', 'Information');
+$off_canvas_phone_number = get_theme_mod('exdos_offcanvas_phone_number', '+ 4 20 7700 1007');
+$off_canvas_email = get_theme_mod('exdos_offcanvas_email', 'hello@exdos.com');
+$off_canvas_location = get_theme_mod('exdos_offcanvas_location', 'Avenue de Roma 158b, Lisboa');
+
+// offcanvas social media
+$off_canvas_follw_us = get_theme_mod('exdos_offcanvas_follow_us_label', 'Follow Us');
+$off_canvas_facebook = get_theme_mod('exdos_offcanvas_facebook', '#');
+$off_canvas_twitter = get_theme_mod('exdos_offcanvas_twitter', '#');
+$off_canvas_linkedin = get_theme_mod('exdos_offcanvas_linkedin', '#');
+$off_canvas_instagram = get_theme_mod('exdos_offcanvas_instagram', '#');
+
+?>
 <div class="tp-offcanvas">
     <div class="tp-offcanvas-wrapper">
         <div class="tp-offcanvas-header d-flex justify-content-between align-items-center mb-90">
             <div class="tp-offcanvas-logo">
-                <a href="index.html"><img
-                        src="<?php echo get_template_directory_uri() ?> /assets/img/logo/logo-black.png" alt=""></a>
+                <a href="index.html"><img src="<?php echo esc_url($off_canvas_logo) ?>" alt=""></a>
             </div>
             <div class="tp-offcanvas-close">
                 <button class="tp-offcanvas-close-toggle"><i class="fal fa-times"></i></button>
@@ -15,36 +40,58 @@
             <nav></nav>
         </div>
         <div class="tp-offcanvas-content mb-50 d-none d-xl-block">
-            <h2 class="tp-offcanvas-title">Hello There!</h2>
-            <p>Lorem ipsum dolor sit amet, consect etur adipiscing elit. </p>
+            <?php
+            if (!empty($off_canvas_text)) {
+                echo '<h2 class="tp-offcanvas-title">' . esc_html($off_canvas_text) . '</h2>';
+            }
+            ?>
+            <?php
+            if (!empty($off_canvas_description)) {
+                echo '<p>' . esc_html($off_canvas_description) . '</p>';
+            }
+            ?>
+
+
         </div>
-        <div class="tp-offcanvas-gallery mb-50">
-            <a class="popup-image" href="assets/img/project/showcase-thumb-01.jpg"><img
-                    src="<?php echo get_template_directory_uri() ?> /assets/img/project/showcase-thumb-01.jpg"
-                    alt=""></a>
-            <a class="popup-image" href="assets/img/project/showcase-thumb-02.jpg"><img
-                    src="<?php echo get_template_directory_uri() ?> /assets/img/project/showcase-thumb-02.jpg"
-                    alt=""></a>
-            <a class="popup-image" href="assets/img/project/showcase-thumb-03.jpg"><img
-                    src="<?php echo get_template_directory_uri() ?> /assets/img/project/showcase-thumb-03.jpg"
-                    alt=""></a>
-            <a class="popup-image" href="assets/img/project/showcase-thumb-04.jpg"><img
-                    src="<?php echo get_template_directory_uri() ?> /assets/img/project/showcase-thumb-04.jpg"
-                    alt=""></a>
-        </div>
-        <div class="tp-offcanvas-info mb-50">
-            <h3 class="tp-offcanvas-sm-title">Information</h3>
-            <span><a href="#">+ 4 20 7700 1007</a></span>
-            <span><a href="#">hello@exdos.com</a></span>
-            <span><a href="#">Avenue de Roma 158b, Lisboa</a></span>
-        </div>
-        <div class="tp-offcanvas-social mb-50">
-            <h3 class="tp-offcanvas-sm-title">Follow Us</h3>
-            <a href="#"><i class="fab fa-facebook-f"></i></a>
-            <a href="#"><i class="fab fa-twitter"></i></a>
-            <a href="#"><i class="fab fa-linkedin-in"></i></a>
-            <a href="#"><i class="fab fa-instagram"></i></a>
-        </div>
+        <?php if (!empty($off_canvas_repeater)) {
+            ?>
+            <div class="tp-offcanvas-gallery mb-50">
+                <?php
+                foreach ($off_canvas_repeater as $item) {
+                    echo '<a class="popup-image" href="' . esc_url($item['image_url']) . '"><img src="' . esc_url($item['image_url']) . '" alt="' . esc_attr($item['image_alt']) . '"></a>';
+                }
+                ?>
+            </div>
+            <?php
+        } ?>
+        <?php
+        if (!empty($off_canvas_phone_number) || !empty($off_canvas_email) || !empty($off_canvas_location)):
+            ?>
+            <div class="tp-offcanvas-info mb-50">
+                <h3 class="tp-offcanvas-sm-title"><?php echo esc_html($off_canvas_information) ?></h3>
+                <span><a href="#"><?php echo esc_html($off_canvas_phone_number) ?></a></span>
+                <span><a href="#"><?php echo esc_html($off_canvas_email) ?></a></span>
+                <span><a href="#"><?php echo esc_html($off_canvas_location) ?></a></span>
+            </div>
+
+        <?php endif; ?>
+        <?php if (!empty($off_canvas_facebook) || !empty($off_canvas_twitter) || !empty($off_canvas_linkedin) || !empty($off_canvas_instagram)): ?>
+            <div class="tp-offcanvas-social mb-50">
+                <h3 class="tp-offcanvas-sm-title"><?php echo esc_html($off_canvas_follw_us) ?></h3>
+                <?php if (!empty($off_canvas_facebook)): ?>
+                    <a href="<?php echo esc_url($off_canvas_facebook) ?>"><i class="fab fa-facebook-f"></i></a>
+                <?php endif; ?>
+                <?php if (!empty($off_canvas_twitter)): ?>
+                    <a href="<?php echo esc_url($off_canvas_twitter) ?>"><i class="fab fa-twitter"></i></a>
+                <?php endif; ?>
+                <?php if (!empty($off_canvas_linkedin)): ?>
+                    <a href="<?php echo esc_url($off_canvas_linkedin) ?>"><i class="fab fa-linkedin-in"></i></a>
+                <?php endif; ?>
+                <?php if (!empty($off_canvas_instagram)): ?>
+                    <a href="<?php echo esc_url($off_canvas_instagram) ?>"><i class="fab fa-instagram"></i></a>
+                <?php endif; ?>
+            </div>
+        <?php endif; ?>
     </div>
 </div>
 <div class="tp-offcanvas-overlay"></div>
