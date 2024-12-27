ff1432e8b411b1cd43a3def72a63b3317d40aae3
tamimhasan19702 <tamimhasancu@gmail.com>
2024-12-28 02:06:30 +0600

    update kirki-footer-functions.php and footer-widget-area.php

--- includes/kirki/kirki-footer-functions.php
+++ includes/kirki/kirki-footer-functions.php
@@ -72,3 +72,73 @@ function exdos_footer_section()
 }
 
 exdos_footer_section();
+
+function exdos_footer_Copyright()
+{
+    if (class_exists('Kirki\Section')) {
+        new \Kirki\Section(
+            'exdos_footer_section',
+            [
+                'title' => esc_html__('Exdos Footer Copyright & Links', 'kirki'),
+                'description' => esc_html__('Exdos Footer Section Description', 'kirki'),
+                'panel' => 'exdos_kirki_panel',
+                'priority' => 160,
+            ]
+        );
+
+        if (class_exists('Kirki\Field')) {
+
+            new \Kirki\Field\Image(
+                [
+                    'settings' => 'exdos_footer_logo',
+                    'label' => esc_html__('Exdos Footer Logo', 'kirki'),
+                    'description' => esc_html__('The saved value will be the URL.', 'kirki'),
+                    'section' => 'exdos_footer_section',
+                    'default' => get_template_directory_uri() . '/assets/img/logo/logo-white.png',
+                ]
+            );
+
+            new \Kirki\Field\URL(
+                [
+                    'settings' => 'exdos_footer_facebook',
+                    'label' => esc_html__('Facebook URL', 'kirki'),
+                    'section' => 'exdos_footer_section',
+                    'default' => 'https://www.facebook.com/',
+                    'priority' => 10,
+                ]
+            );
+
+            new \Kirki\Field\URL(
+                [
+                    'settings' => 'exdos_footer_instagram',
+                    'label' => esc_html__('Instagram URL', 'kirki'),
+                    'section' => 'exdos_footer_section',
+                    'default' => 'https://www.instagram.com/',
+                    'priority' => 10,
+                ]
+            );
+
+            new \Kirki\Field\URL(
+                [
+                    'settings' => 'exdos_footer_linkedin',
+                    'label' => esc_html__('LinkedIn URL', 'kirki'),
+                    'section' => 'exdos_footer_section',
+                    'default' => 'https://www.linkedin.com/',
+                    'priority' => 10,
+                ]
+            );
+
+            new \Kirki\Field\URL(
+                [
+                    'settings' => 'exdos_footer_twitter',
+                    'label' => esc_html__('Twitter URL', 'kirki'),
+                    'section' => 'exdos_footer_section',
+                    'default' => 'https://twitter.com/',
+                    'priority' => 10,
+                ]
+            );
+
+
+        }
+    }
+}
--- template-parts/footer/footer-widget-area.php
+++ template-parts/footer/footer-widget-area.php
@@ -2,21 +2,35 @@
     <div class="row">
 
         <div class="col-xl-3">
+            <?php if (is_active_sidebar('footer-1')): ?>
+
             <?php dynamic_sidebar('footer-1') ?>
+
+            <?php endif; ?>
         </div>
 
-
         <div class="col-xl-3 col-lg-4 col-md-4">
+            <?php if (is_active_sidebar('footer-2')): ?>
+
             <?php dynamic_sidebar('footer-2') ?>
+
+            <?php endif; ?>
         </div>
 
-
         <div class="col-xl-3 col-lg-4 col-md-4">
+            <?php if (is_active_sidebar('footer-3')): ?>
+
             <?php dynamic_sidebar('footer-3') ?>
+
+            <?php endif; ?>
         </div>
 
         <div class="col-xl-3 col-lg-4 col-md-4">
+            <?php if (is_active_sidebar('footer-4')): ?>
+
             <?php dynamic_sidebar('footer-4') ?>
+
+            <?php endif; ?>
         </div>
     </div>
 </div>
