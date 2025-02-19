e786137f8263e062036188e21a7995174bcd091b
tamimhasan19702 <tamimhasancu@gmail.com>
2025-02-19 23:19:57 +0600

    update archive-product.php and result-count.php

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -38,6 +38,29 @@ do_action( 'woocommerce_before_main_content' );
  */
 do_action( 'woocommerce_shop_loop_header' );
 
+?>
+
+<div class="tp-shop-top">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="tp-shop-top-left mb-25 mt-10">
+                <?php woocommerce_result_count(); ?>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="tp-product-top-select d-flex justify-content-end mb-30">
+                <?php woocommerce_catalog_ordering(); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php 
+
+
+
+
+
 if ( woocommerce_product_loop() ) {
 
 	/**
